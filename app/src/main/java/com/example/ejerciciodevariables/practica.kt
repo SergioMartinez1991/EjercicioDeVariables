756872fb4e036fb2a73efc1c84121fe4e904df2e
SergioMartinez1991 <sergiomartinezcolumna@gmail.com>
2024-07-10 11:15:49 -0600

    first commit

---
+++ app/src/main/java/com/example/ejerciciodevariables/practica.kt
@@ -0,0 +1,20 @@
+package com.example.ejerciciodevariables
+
+fun main (){
+    // Valores Enteros - Int
+    val age:Int = 33
+    //long contienen muchos numeros
+    val exampleLong:Long = 30
+    //valores flotantes
+    val exampleFloat:Float = 30.5f
+    //Double
+    val doubleExample:Double = 45673.6789
+    //variables alfanumericas
+    val charExample01:Char = '@'
+    val charExample02:Char = '2'
+    //booleanos
+    val booleanEjemplo:Boolean = true
+
+        print( booleanEjemplo)
+
+}
